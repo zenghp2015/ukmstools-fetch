3cabf3e137201baaedae6c50ea1bb67911749d83
zengh2015 <zenghp2015@gmail.com>
2021-11-07 00:07:36 +0800

    Todo:

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,7 @@ module.exports = {
     },
   },
   // https://cli.vuejs.org/zh/guide/build-targets.html#vue-vs-js-ts-%E5%85%A5%E5%8F%A3%E6%96%87%E4%BB%B6
+  // 不具名导出
   configureWebpack: {
     output: {
       libraryExport: 'default'
