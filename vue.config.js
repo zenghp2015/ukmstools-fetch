b46a12f857c3edf855c93c038580ccddb964a074
zengh2015 <zenghp2015@gmail.com>
2021-11-06 23:57:05 +0800

    add: 编译包

--- vue.config.js
+++ vue.config.js
@@ -15,17 +15,4 @@ module.exports = {
       chunks: ["chunk-vendors", "chunk-common", "index"],
     },
   },
-  // 扩展 webpack 配置，使 packages 加入编译
-  chainWebpack: (config) => {
-    config.module
-      .rule("js")
-      .include.add("/src")
-      .end()
-      .use("babel")
-      .loader("babel-loader")
-      .tap((options) => {
-        // 修改它的选项...
-        return options;
-      });
-  },
 };
