141d3e389aa90eec1b12c7c4ab34d9b76cbded02
zengh2015 <zenghp2015@gmail.com>
2021-11-07 00:04:58 +0800

    Update: 不具名导出

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   pages: {
     index: {
-      // page 的入口
+      // 示例入口
       entry: "examples/main.js",
-      // 模板来源
       template: "public/index.html",
-      // 在 dist/index.html 的输出
       filename: "index.html",
-      // 当使用 title 选项时，
-      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: "Index Page",
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
+      title: "v-fetch examples",
       chunks: ["chunk-vendors", "chunk-common", "index"],
     },
   },
+  // https://cli.vuejs.org/zh/guide/build-targets.html#vue-vs-js-ts-%E5%85%A5%E5%8F%A3%E6%96%87%E4%BB%B6
+  configureWebpack: {
+    output: {
+      libraryExport: 'default'
+    }
+  }
 };
